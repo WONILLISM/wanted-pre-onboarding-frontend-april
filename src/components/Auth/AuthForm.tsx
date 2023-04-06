5499105fb95d010a3e17483ecb90b83a631c5f65
WONILLISM <wonillism@gmail.com>
2023-04-06 13:19:17 +0900

    [feat] #6 로그인 회원가입 페이지 이동 처리

--- src/components/Auth/AuthForm.tsx
+++ src/components/Auth/AuthForm.tsx
@@ -1,16 +1,61 @@
 import React from "react";
-import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
+import styled, { css } from "styled-components";
 
-const authTypeText = {
-  login: "LOGIN",
-  register: "REGISTER",
-};
+const AuthTitle = styled.div<{ authtype: "login" | "register" }>`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 
-const AuthTitle = styled.div`
-  font-size: 2rem;
-  font-weight: 500;
-  letter-spacing: 0.4rem;
-  color: #199fb1;
+  & .login-text {
+    &:hover {
+      cursor: pointer;
+      transform: scale(1.1);
+      transition: all 0.2s ease-in-out;
+    }
+  }
+
+  & .register-text {
+    &:hover {
+      cursor: pointer;
+      transform: scale(1.1);
+      transition: all 0.2s ease-in-out;
+    }
+  }
+
+  & .login-text {
+    ${(props) =>
+      props.authtype === "login"
+        ? css`
+            font-size: 2rem;
+            font-weight: 500;
+            letter-spacing: 0.4rem;
+            color: #199fb1;
+          `
+        : css`
+            font-size: 1rem;
+            font-weight: 500;
+            letter-spacing: 0.1rem;
+            color: #ff6700;
+          `}
+  }
+
+  & .register-text {
+    ${(props) =>
+      props.authtype === "register"
+        ? css`
+            font-size: 2rem;
+            font-weight: 500;
+            letter-spacing: 0.4rem;
+            color: #199fb1;
+          `
+        : css`
+            font-size: 1rem;
+            font-weight: 500;
+            letter-spacing: 0.1rem;
+            color: #ff6700;
+          `}
+  }
 `;
 
 const RootStyle = styled.div`
@@ -24,9 +69,28 @@ interface AuthFormProps {
 }
 
 const AuthForm = ({ authType, children }: AuthFormProps) => {
+  const navigate = useNavigate();
+
   return (
     <RootStyle>
-      <AuthTitle>{authTypeText[authType]}</AuthTitle>
+      <AuthTitle authtype={authType}>
+        <div
+          className="login-text"
+          onClick={() => {
+            navigate("/signin");
+          }}
+        >
+          LOGIN
+        </div>
+        <div
+          className="register-text"
+          onClick={() => {
+            navigate("/signup");
+          }}
+        >
+          REGISTER
+        </div>
+      </AuthTitle>
       {children}
     </RootStyle>
   );
